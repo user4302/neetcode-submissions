9334eb6a5416f4d9f94e4f6c8445f124ef254dc7
AEDZ <32593782+user4302@users.noreply.github.com>
2026-06-02 00:42:53 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        // this will be used to map each number in the array with its index
+        const map = {}
+
+        for (let i = 0; i < nums.length; i++) {
+            // the difference is basically the missing number needed to reach the target value
+            const difference = target - nums[i]
+
+            // if the differnece that we need is in our map, then we found the missing value
+            if (map[difference] !== undefined) {
+                // so we return the current index, adn the index of the missing value that helps the current index become the target
+                return [i, map[difference]]
+            }
+
+            // else we simply add the current value and its index to the map to use in the next iteration
+            map[nums[i]] = i
+        }
+    }
+}
