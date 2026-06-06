d63ccdc6d962db28aa6c735dfd4adfb59c164564
AEDZ <32593782+user4302@users.noreply.github.com>
2026-06-06 14:52:23 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        // create a map to store a sorted string as the key
+        // and an array of matches in an array as the value
+        let map = {};
+
+        if (strs.length < 0) return [[""]];
+        // for loop to iterate array
+        for (let i = 0; i < strs.length; i++) {
+            const sortedString = strs[i].split('').sort().join('');
+
+            // if the map has a key matching the current sorted string
+            // if(map.has(sortedString)){
+            // AN IF CONDITION IS NOT NEEDED, AS BOTH CONDITIONS ARE THE SAME
+
+            // create a KV pair where the K is the sorted string
+            // an its V is a push to the array of the unsorted string
+            (map[sortedString] = map[sortedString] || []).push(strs[i]);
+        }
+
+        // return final values as an array
+        return (Object.values(map));
+    }
+}
