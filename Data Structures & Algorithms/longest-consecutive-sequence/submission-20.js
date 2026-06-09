110c1c22b0958e4c00a483d5faab58d9954ce42e
AEDZ <32593782+user4302@users.noreply.github.com>
2026-06-09 16:05:28 +0530

    Add: longest-consecutive-sequence - submission-20

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-20.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        // add all values into a set to isolate unique values
+        // and check value existemce without time complexity
+        let numsSet = new Set(nums);
+        let longestConsecutiveSequence = 0;
+        // console.log("numsSet", numsSet);
+        if (numsSet.size < 1) return 0;
+        if (numsSet.size < 2) return 1;
+
+        // loop through items
+        for (const num of numsSet) {
+            // simply check the opposite 
+            // and eliminate the need for
+            // an else if or else block
+            if (!numsSet.has(num - 1)) {
+                let currentLength = 1;
+                // simply use the currentLength 
+                // and num from the parent
+                // instead of making a new variable for num
+                while (numsSet.has(num + currentLength)) {
+                    currentLength++;
+                }
+
+                longestConsecutiveSequence = Math.max(
+                    longestConsecutiveSequence, currentLength
+                );
+            }
+        }
+        return longestConsecutiveSequence;
+    }
+}
