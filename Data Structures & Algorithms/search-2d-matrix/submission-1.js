a8d768db74762650ffcb7ff9a2c1781f42d28510
AEDZ <32593782+user4302@users.noreply.github.com>
2026-06-11 11:02:58 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        // NOTE: "non-decreasing" means that while it increases-
+        // it can also have duplicates (does not decrease)
+
+        // index[n][0] > index[n-1][0]
+
+        // since we know the target,
+        // and we know that each childs int-
+        // starts greater than the previous childs ending int
+
+        // we only need to check perhaps the first int of each child
+        // and the moment that we find one thats greater than the target-
+        // we can search the previous child for the target
+
+        let found = false
+        for (let row = 0; row < matrix.length; row++) {
+            // if first val >= target and last val <= target
+            console.log('matrix[row][0]>=target',matrix[row][0]>=target)
+            console.log('matrix[row][matrix[row].length-1]<=target',matrix[row][matrix[row].length-1]<=target)
+            
+            if(matrix[row][0]<=target && matrix[row][matrix[row].length-1]>=target){
+                found = matrix[row].includes(target)
+            }
+        }
+        return found
+    }
+}
